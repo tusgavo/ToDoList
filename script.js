d12acccbedb5b58cadea5c094805644909f514dd
tusgavo <gustavoooliveira022@gmail.com>
2021-01-14 16:28:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// Crie um botão "Fechar" e anexe-o a cada item da lista
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Clique no botão Fechar para ocultar o item da lista atual
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Adicionar um símbolo "marcado" ao clicar em um item da lista
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+// Crie um novo item da lista ao clicar no botão "Adicionar"
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    Swal.fire({
+      title: "O campo está vazio!",
+      icon: "error",
+      confirmButtonText: "Ok",
+    });
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
